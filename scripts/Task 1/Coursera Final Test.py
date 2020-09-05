f853a1a094bf0caac4a1f83156c04d7de7920407
Daniel Vareda <daniel.varedav@gmail.com>
2020-09-05 10:26:33 -0300

    added task 1 code

---
+++ scripts/Task 1/Coursera Final Test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook For Coursera Final Assignment
+
+# This Notebook contains the dataset extraction and analysis, as well as machine learning models, to predict car crashs in CANADA's roads
+
+# In[1]:
+
+
+import os
+os.chdir('/home/ec2-user/SageMaker/')
+
+get_ipython().system('pip install -r ./sales_utils/requirements.txt --quiet')
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
